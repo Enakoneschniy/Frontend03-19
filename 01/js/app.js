65380128643f77263f0afc0338b40465ef1a73a9
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2019-03-26 11:48:11 +0200

    lesson 01 intro

---
+++ 01/js/app.js
@@ -0,0 +1,30 @@
+var name;
+name = 'Petya';
+
+const userName = 'Vasya'; // string
+const userName1 = "Petya"; // string
+const userName2 = `Petya ${userName}`; // string
+console.log(userName2);
+const age = 34; // number
+const age1 = 34.8; // number
+const test = undefined; // undefined
+const test1 = null; // null / object
+const isTrue = true; // boolean
+const obj = {};
+/*
+  gfgh
+sgfdf
+fsdg
+*/
+let user_name = 'Vasya ';
+{
+    console.log(user_name);
+    // let user_name = 'Petya';
+    console.log(user_name);
+
+    {
+        let user_name = 'Petya';
+    }
+}
+
+
